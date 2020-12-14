b5d1998fc4edae0859cba08ba3544f0f3de6a17e
Reign <75963171+Reign714@users.noreply.github.com>
2020-12-13 21:07:10 -0700

    Update commented.js

--- commented.js
+++ commented.js
@@ -1,12 +1,4 @@
-/*client.on("guildCreate", () => {
-  client.user.setActivity(` ${config.prefix}help`);
-});
-
-client.on("guildDelete", () => {
-  client.user.setActivity(` ${config.prefix}help`);
-});*/
-
-/*client.on('guildMemberAdd', async member => {
+client.on('guildMemberAdd', async member => {
 	const channel = member.guild.channels.find(ch => ch.name === 'welcome');
 	if (!channel) return;
 
@@ -40,9 +32,9 @@ client.on("guildDelete", () => {
 	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
 
 	channel.send(`Welcome to the server, ${member}!`, attachment);
-});*/
+});
 
-/*client.on("channelCreate", channel => {
+client.on("channelCreate", channel => {
   logChannel.send(`${memberJoined} joined, invited by ${memberJoined.invite}`);
 });
 
@@ -53,38 +45,9 @@ client.on("guildMemberAdd", channel => {
 client.on("guildMemberAdd", memberJoined => {
   const inviteListChannel = client.channels.get("747585757954310245");
   inviteListChannel.send(`${memberJoined} joined, invited by ${memberJoined.invite}`);
-});*/
+});
 
-//for (var i = 0; i < banned.length; i++) {
-    //if (banned.some(word => message.content.includes(word))) {
-    /*if (filter.isProfane(message.content)) {
-
-      const member = message.author;
-      const channel = message.client.channels.get("754132932708270151");
-
-      const embed = new Discord.RichEmbed()
-        if (message.member.highestRole.color.toString() === "0") {
-          embed.setColor("#95a5a6")
-        } else {
-          embed.setColor(message.member.highestRole.color)
-        }
-        embed.addField("Sender", message.member.displayName, true)
-        .addField("Content", message.content, false)
-        .addField("Channel", `#${message.channel.name}`, false)
-        .addField("When", moment(message.createdAt).format('MM/DD/YYYY, h:mm:ss a'))
-        .setAuthor(message.guild.name, message.guild.iconURL)
-        .setTitle("Message Deleted")
-        .setThumbnail(message.author.displayAvatarURL)
-        .setTimestamp()
-      channel.send(embed);
-
-      message.delete();
-      var reply = await message.reply("Please refrain from using that kind of language here!");
-      reply.delete(5000);
-    }*/
-  //}
-
-  /*client.on("messageUpdate", async (oldMessage, newMessage) => {
+  client.on("messageUpdate", async (oldMessage, newMessage) => {
   if (filter.isProfane(newMessage.content)) {
     const member = newMessage.author;
     const channel = newMessage.client.channels.get("754132932708270151");
@@ -107,4 +70,4 @@ client.on("guildMemberAdd", memberJoined => {
     var reply = await newMessage.reply("Please refrain from using that kind of language here!");
     reply.delete(5000);
   }
-});*/
+});
